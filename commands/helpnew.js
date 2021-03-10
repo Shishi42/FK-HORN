419fd2201453a4069c937fa79bcae3c13a5684d4
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-10 16:48:53 +0100

    Add files via upload

---
+++ commands/helpnew.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js")
+const config = require("../config.json");
+const collections = require("../collections.json");
+
+module.exports.run = async (bot, message, args) => {
+  return message.channel.send('New sounds : !['+collections.new+']');
+}
+
+module.exports.config = {
+  name: "helpnew",
+  aliases: ["hn","helpnew"]
+}
