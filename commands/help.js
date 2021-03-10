71760df580f6884acf92962b8145ec6811764338
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-10 17:11:05 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,14 @@ const config = require("../config.json");
 const collections = require("../collections.json");
 
 module.exports.run = async (bot, message, args) => {
-  message.channel.send('Airhorn usage : !['+collections.list+']');
+
+  str = "```\n"  
+  for (i = 0; i < collections.list.length; i++){
+    str += "!"+collections.list[i]+"\n"
+  }
+  str += "```"
+
+  message.channel.send('Airhorn sound list ('+collections.list.length+') : \n'+str);
   return message.channel.send('Command List : [google,help,helpnew,ping,say,uptime]');
 }
 
