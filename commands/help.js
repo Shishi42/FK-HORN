414c5ffcc66018b47a1d35528e96e0d343d1899b
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-10 16:45:58 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const collections = require("../collections.json");
 
 module.exports.run = async (bot, message, args) => {
   message.channel.send('Airhorn usage : !['+collections.list+']');
-  return message.channel.send('Command List : [google,help,ping,say,uptime]');
+  return message.channel.send('Command List : [google,help,helpnew,ping,say,uptime]');
 }
 
 module.exports.config = {
