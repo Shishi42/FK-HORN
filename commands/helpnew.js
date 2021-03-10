9ebbe7c4acade7f4ea6cb5e2b0e34ceece2b69ab
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-10 17:06:32 +0100

    Update helpnew.js

--- commands/helpnew.js
+++ commands/helpnew.js
@@ -3,7 +3,13 @@ const config = require("../config.json");
 const collections = require("../collections.json");
 
 module.exports.run = async (bot, message, args) => {
-  return message.channel.send('New sounds ('+collections.new.length+') : !['+collections.new+']');
+  str = "```\n"
+  collections.new.forEach(elm){
+    str += "!"+elm+"\n"
+  }
+  str += "```"
+  
+  return message.channel.send('New sounds ('+collections.new.length+') : \n'+str);
 }
 
 module.exports.config = {
