91ad05d478ca0f97ae829e35a1d5c120409b1fff
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 08:22:07 +0100

    Update airhorn.js

--- commands/airhorn.js
+++ commands/airhorn.js
@@ -49,6 +49,10 @@ module.exports.run = async (bot, message, args) => {
 
   function stats(collection_name){
     if(fs.existsSync("stats.json")) stats = jsonfile.readFileSync("stats.json")
+    else{
+      fs.appendFile("stats.json", "{}", function(err) {})
+      stats = jsonfile.readFileSync("stats.json")
+    }
     if(stats[collection_name] == undefined) stats[collection_name] = 1
     else stats[collection_name] += 1    
     fs.appendFile("stats.json", JSON.stringify(stats, null, 4), function(err) {})
