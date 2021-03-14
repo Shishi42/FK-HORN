fbabb61408bc1680ecdba94517e7a0d5d58a0dc0
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 08:59:48 +0100

    Update reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -23,7 +23,7 @@ module.exports.run = async (bot, message, args) => {
    isSomethingReset = true
   }
   
-  if(isSomethingReset) message.channel.send("you have reseted "+args[0]+".")
+  if(isSomethingReset) message.channel.send(args[0]+" successfully reset.")
 }
 
 module.exports.config = {
