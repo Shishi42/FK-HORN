c387c6bc2f5d61975af1173661ca4a64c3773d5c
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 10:36:00 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
       
     embed.setAuthor((args[0].charAt(0).toUpperCase() + args[0].slice(1))+" Command Help", bot.user.displayAvatarURL(), "")
     embed.setDescription([
-      "**> Description:** "+bot.commands.get(args[0]).config.desc",
+      `**> Description:** ${bot.commands.get(args[0]).config.desc}`,
       "**> Aliases:** "+bot.commands.get(args[0]).config.aliases",
       "**> Possible Arguments:** "+bot.commands.get(args[0]).config.args"
     ])
