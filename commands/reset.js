0c77bef3bf94d52cb450f79b25ae36875fbae7e6
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 09:31:18 +0100

    Update reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -30,6 +30,6 @@ module.exports.run = async (bot, message, args) => {
 module.exports.config = {
   name: "reset",
   aliases: ["rst","rs"],
-  desc: "Reset the arg (only for bot owner)",
-  args: ["logs","stats","all"]
+  args: ["logs","stats","all"],
+  desc: "Reset the arg (only for bot owner)"
 }
