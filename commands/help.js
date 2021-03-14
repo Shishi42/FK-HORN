56c7d16fbea54a5656f8325e856183e3695ae415
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 09:59:57 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,6 +9,8 @@ module.exports.run = async (bot, message, args) => {
   commands = Array.from(bot.commands.keys())
   
   if(commands.includes(args[0])){
+      console.log(bot.commands)
+    console.log(bot.commands.get(args[0]))
      str = "```\n"
      str += "command : "+args[0]+"\n"
      str += "aliases : "+bot.commands.get(args[0]).config.aliases+"\n"
