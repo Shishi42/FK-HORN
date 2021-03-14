1b6e1125674139dc839e9498c758c3ac53b225f8
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 09:33:26 +0100

    Update fkhorn.js

--- fkhorn.js
+++ fkhorn.js
@@ -23,6 +23,7 @@ fs.readdir("./commands/", (err, files) => {
   }
 
   jsfile.forEach((f, i) => {
+    console.log(f)
     let pull = require(`./commands/${f}`)
     bot.commands.set(pull.config.name, pull)
     pull.config.aliases.forEach(alias => {
