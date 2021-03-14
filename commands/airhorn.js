0fdc2cc876b19bfafe075fc788e2c7488bfed5f6
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 08:06:45 +0100

    Update airhorn.js

--- commands/airhorn.js
+++ commands/airhorn.js
@@ -51,7 +51,8 @@ module.exports.run = async (bot, message, args) => {
     if(fs.existsSync("stats.json")) stats = jsonfile.readFileSync("stats.json")
     if(stats[collection_name] == undefined) stats[collection_name] = 1
     else stats[collection_name] += 1
-    console.log(typeof stats);
+    
+    console.log(stats);
     
     stats.sort(function(a, b) {
       return stats[a] > stats[b];
