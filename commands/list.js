0b90e7d8b8b2e9a78d34fd1e8dd5d9a4bc4e0a93
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 11:47:10 +0100

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -6,12 +6,13 @@ const path = require('path')
 
 module.exports.run = async (bot, message, args) => {
     
-  str_snd = "```\n)"
+  str_snd = "```\n"
   for (i = 0; i < bot.sound_collections.length; i++){
     temp = fs.readdirSync(path.join(__dirname, "/audio/"+bot.sound_collections[i]))
     if(temp.length > 1) str_snd += "!"+bot.sound_collections[i]+" ("+temp.length+" sounds)\n"
     else str_snd += "!"+bot.sound_collections[i]+"\n"
   }
+  str_snd += "```"
   message.channel.send('Airhorn sound list ('+bot.sound_collections.length+') : \n'+str_snd)
 
   return message.delete()
