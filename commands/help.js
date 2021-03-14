63084ed86675c24edeb1fe4886cb05ec3c7543d5
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 10:32:50 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,15 +8,20 @@ module.exports.run = async (bot, message, args) => {
 
   commands = Array.from(bot.commands.keys())
   
-  if(commands.includes(args[0])){
-    
-    const embed = new Discord.MessageEmbed()
+  const embed = new Discord.MessageEmbed()
       .setColor('#553380')
-      .setAuthor(bot.user.username+' Help Menu', bot.user.displayAvatarURL(), bot.user.displayAvatarURL())
+      .setAuthor(bot.user.username+' Help Menu', bot.user.displayAvatarURL(), "")
       .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL())
       .setTimestamp();
-    
-    
+  
+  if(commands.includes(args[0])){
+      
+    embed.setAuthor((args[0].charAt(0).toUpperCase() + args[0].slice(1))+" Command Help", bot.user.displayAvatarURL(), "")
+    embed.setDescription([
+      `**> Description:** ${bot.commands.get(args[0]).config.desc}`  
+      `**> Aliases:** ${bot.commands.get(args[0]).config.aliases}`
+      `**> Possible Arguments:** ${bot.commands.get(args[0]).config.args}`
+    ])
     
     
      str = "```\n"
