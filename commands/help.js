6e7f4f1bf43f221f1d6565c1dc3094af26a3799a
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-14 09:38:36 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,9 +12,9 @@ module.exports.run = async (bot, message, args) => {
     if(commands.includes(args[0])){
        str = "```\n"
        str += "command : "+args[0]
-       str += "aliases : "+bot.commands.get(bot.aliases.get(args[0]))
-       str += "description : "+bot.commands.get(bot.desc.get(args[0]))
-       str += "possible argument : "+bot.commands.get(bot.args.get(args[0]))
+       str += "aliases : "+bot.commands.get(bot.aliases.get(args[0]))+"\n"
+       str += "description : "+bot.commands.get(bot.desc.get(args[0]))+"\n"
+       str += "possible argument : "+bot.commands.get(bot.args.get(args[0]))+"\n"
        str += "```"
        message.channel.send(str)
     }
