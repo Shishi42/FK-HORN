761fc76dc75215e6e381dfa671b6f85da683d633
Ben R <37541779+Shishi42@users.noreply.github.com>
2021-03-23 22:07:20 +0100

    Update fkhorn.js

--- fkhorn.js
+++ fkhorn.js
@@ -47,7 +47,7 @@ bot.on("message", async message => {
 
   if(bot.sound_collections.includes(cmd.slice(prefix.length))){
     let airhorn_number = -1
-    console.log(args[0]))
+    console.log(args[0])
     if(Number.isInteger(args[0])) airhorn_number = args[0]
         console.log(airhorn_number)
     let commandFile = bot.commands.get("airhorn")
