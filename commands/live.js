bcfea3606312e44c4f01bbe4933365251b58ee71
shishi42 <benriv4272@gmail.com>
2021-05-14 04:46:47 +0200

    fix

--- commands/live.js
+++ commands/live.js
@@ -4,23 +4,28 @@ const config = require("../config.json")
 module.exports.run = async (bot, message, args) => {
   if(message.member != config.bot_owner) return message.reply("You must be bot owner")
 
-  if(args[0] == "on"){
+  if(args[0].toUpperCase() == "ON"){
     bot.live_mode = true
     message.channel.send("```Live-mode set to ON```")
-  }else if(args[0] == "off"){
+    if(args[1] != undefined){
+      bot.stream_channel = args[1]
+      voice_channel = bot.channels.cache.get(bot.stream_channel)
+      message.channel.send("```Stream Channel set to "+bot.stream_channel+" | "+voice_channel.guild.name+"/"+voice_channel.name+"```")
+    }
+  }else if(args[0].toUpperCase() == "OFF"){
     bot.live_mode = false
     message.channel.send("```Live-mode set to OFF```")
+    if(args[1] != undefined){
+      bot.stream_channel = args[1]
+      voice_channel = bot.channels.cache.get(bot.stream_channel)
+      message.channel.send("```Stream Channel set to "+bot.stream_channel+" | "+voice_channel.guild.name+"/"+voice_channel.name+"```")
+    }
   }else{
     bot.stream_channel = args[0]
     voice_channel = bot.channels.cache.get(bot.stream_channel)
     message.channel.send("```Stream Channel set to "+bot.stream_channel+" | "+voice_channel.guild.name+"/"+voice_channel.name+"```")
   }
-  if(args[1] != undefined){
-    bot.stream_channel = args[1]
-    voice_channel = bot.channels.cache.get(bot.stream_channel)
-    message.channel.send("```Stream Channel set to "+bot.stream_channel+" | "+voice_channel.guild.name+"/"+voice_channel.name+"```")
-  }
-
+  
   return message.delete()
 }
 
