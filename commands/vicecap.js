d0d91552af1edd5c037f80efbce3f307842d3ae0
shishi42 <benji.riviere72@hotmail.fr>
2021-10-01 17:13:59 +0200

    up

--- commands/vicecap.js
+++ commands/vicecap.js
@@ -34,36 +34,56 @@ module.exports.run = async (bot, message, args) => {
 
   var ban = Array.from(message.mentions.users.keys())
 
-  do{
-    vc1 = message.guild.members.cache.random()
-  }
-  while(vc1.user.bot == true || vc1._roles.includes('474666848462307338') || vc1._roles.includes('474666806192111628') || ban.includes(vc1.user.id))
+  if(!isNaN(parseInt(args[0])) && parseInt(args[0]) <= 5 && parseInt(args[0]) > 0) nbTir = parseInt(args[0])
+  else nbTir = 2
 
-  if(vc1.nickname == null) name = vc1.user.username
-  else name = vc1.nickname
-  embed.addField(name, '@'+vc1.user.username+'#'+vc1.user.discriminator)
+  elu = []
 
-  if(args[0] != '1'){
+  console.log('Début Tirage ('+nbTir+')')
+
+  for(i = 0; i < nbTir; i++){
     do{
-      vc2 = message.guild.members.cache.random()
+      tir = message.guild.members.cache.random()
+
+      if(tir.user.bot == true) console.log('FAIL : '+tir.user.username+' -> BOT')
+      else if(tir._roles.includes('474666848462307338')) console.log('FAIL : '+tir.user.username+' -> Déjà Capitaine')
+      else if(tir._roles.includes('474666806192111628')) console.log('FAIL : '+tir.user.username+' -> Déjà Vice-Capitaine')
+      else if(elu.includes(tir.user.id)) console.log('FAIL : '+tir.user.username+' -> Déjà Elu')
+      else if(ban.includes(tir.user.id)) console.log('FAIL : '+tir.user.username+' -> BAN')
+      else console.log('HIT : '+tir.user.username)
     }
-    while(vc2.user.bot == true || vc2._roles.includes('474666848462307338') || vc2._roles.includes('474666806192111628') || ban.includes(vc2.user.id) || vc1.id == vc2.id);
-
-    if(vc2.nickname == null) name = vc2.user.username
-    else name = vc2.nickname
-    embed.addField(name, '@'+vc2.user.username+'#'+vc2.user.discriminator)
-
-    logs(message, vc1.user.username, vc2.user.username)
-  }
-  else{
-    logs(message, vc1.user.username, "NULL")
+    while(tir.user.bot == true || tir._roles.includes('474666848462307338') || tir._roles.includes('474666806192111628') || ban.includes(tir.user.id) || elu.includes(tir.user.id))
+    elu.push(tir.user.id)
   }
 
-  function logs(message, vc1, vc2){
+  elu.forEach(id => {
+    usr = message.guild.members.cache.get(id)
+    if(usr.nickname == null) name = usr.user.username
+    else name = usr.nickname
+    embed.addField(name, '@'+usr.user.username+'#'+usr.user.discriminator)
+  });
+
+  logs(message, elu, ban)
+
+  function logs(message, tirs, bans){
     temp_date = message.createdAt
     date = "["+temp_date.getHours()+":"+temp_date.getMinutes()+":"+temp_date.getSeconds()+" - "+temp_date.getDate()+" "+months[temp_date.getMonth()]+" "+temp_date.getFullYear()+"]"
 
-    logs = 'Tirage vice-capitaine par '+message.author.username+': #1 '+vc1+', #2 '+vc2+' --> fait le '+date+'.'
+    temp_name = []
+    elu.forEach(tir => {
+      usr = message.guild.members.cache.get(tir).user
+      temp_name.push('@'+usr.username+'#'+usr.discriminator)
+    });
+
+    temp_ban = []
+    bans.forEach(ban => {
+      usr = message.guild.members.cache.get(ban).user
+      temp_ban.push('@'+usr.username+'#'+usr.discriminator)
+    });
+    if(temp_ban.length == 0) temp_ban.push("NO BAN")
+
+    logs = 'Tirage vice-capitaine par '+message.author.username+': '+temp_name+' --> fait le '+date+'. BAN : '+temp_ban
+
     fs.appendFileSync('logs.txt', logs+"\n", function (err){});
 
     console.log(logs);
@@ -76,7 +96,7 @@ module.exports.run = async (bot, message, args) => {
 module.exports.config = {
   name: "vicecap",
   aliases: ["vc","vcp"],
-  args: ["1","@removed_member"],
-  usage: ["vicecap <1> <args>"],
-  desc: "Choose a vice-captain from a random non-captain non-removed non-bot member of the server."
+  args: ["number_of_draw","@removed_member"],
+  usage: ["vicecap <args>"],
+  desc: "Choose a number of vice-captain from a random non-captain non-removed non-bot member of the server."
 }
