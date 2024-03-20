6c16f11d93c624fae14d6a84b6837169e81facc6
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-03-20 17:27:50 +0100

    test

--- fkhorn.js
+++ fkhorn.js
@@ -61,8 +61,8 @@ new cron.CronJob('00 * * * * *', () => {
     filtered = games.filter((game) => game.title.toLowerCase().includes("pokémon") || game.title.toLowerCase().includes("イナズマ"))
     filtered.length ?
       filtered.forEach(game => {
-        res = `:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`
-        bot.channels.fetch("423919360902692866").then(chan => await chan.send(res))
+        await res = `:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`
+        bot.channels.fetch("423919360902692866").then(chan => chan.send(res))
         //bot.channels.fetch("662216228340760596").then(chan => chan.send(`:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`))
         //webhook.send(`:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`)
       }) : bot.channels.fetch("1219989241782599801").then(chan => chan.send(`:flag_eu: \`${date_eu}\` : no hit for **Inazuma Eleven** on the **European eShop**`))
