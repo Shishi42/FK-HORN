847a3a64b159ec2d49f7fead25de6d6676942abb
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-03-20 14:04:02 +0100

    test

--- fkhorn.js
+++ fkhorn.js
@@ -51,7 +51,8 @@ bot.player.events.on("playerStart", async (queue, track) => {
 })
 
 new cron.CronJob('00 * * * * *', () => {
-  date = Date().getDate()+'/'+(Date().getMonth()+1)+'/'+Date().getFullYear()+' - '+Date().getHours()+':'+Date().getMinutes()
+  date = new Date()
+  date_str = date.getdate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' - '+date.getHours()+':'+date.getMinutes()
   getGamesEurope().then(games => 
     filtered = games.filter(game => game.title.toLowerCase().includes("inazuma"))) 
     filtered.length ?
