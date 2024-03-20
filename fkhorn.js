69369b75b870b983affb2950a9fe4a580fd9906e
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-03-20 14:07:46 +0100

    tets

--- fkhorn.js
+++ fkhorn.js
@@ -55,12 +55,12 @@ new cron.CronJob('00 * * * * *', () => {
   console.log(date)
   date_str = date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' - '+date.getHours()+':'+date.getMinutes()
   getGamesEurope().then(games => 
-    filtered = games.filter(game => game.title.toLowerCase().includes("inazuma"))) 
+    filtered = games.filter((game) => game.title.toLowerCase().includes("inazuma"))
     filtered.length ?
       filtered.forEach(game => {
         bot.channels.get("1219989241782599801").then(chan => chan.send(`\`${date}\` **${game.title}** was found on the European eShop. @everyone`))
       }) :
-      bot.channels.get("1219989241782599801").then(chan => chan.send(`\`${date}\` No hit for Inazuma Eleven on the European eShop.`))
+      bot.channels.get("1219989241782599801").then(chan => chan.send(`\`${date}\` No hit for Inazuma Eleven on the European eShop.`)))
 
 }).start()
 
