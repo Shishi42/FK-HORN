983cc5d32269dbf0034f4ed04cc11add9bfa4bb7
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-03-20 13:38:03 +0100

    test

--- fkhorn.js
+++ fkhorn.js
@@ -60,4 +60,4 @@ bot.login(config.token)
 getGamesEurope().then(games => games.filter(game => game.title.toLowerCase().includes("inazuma")).size ? console.log("true") : console.log("false"))
 getGamesEurope().then(games => games.filter(game => game.title.toLowerCase().includes("pokémon")).size ? console.log("true") : console.log("false"))
 
-getGamesEurope().then(games => console.log(games.filter(game => game.title.toLowerCase().includes("pokémon")).size))
+getGamesEurope().then(games => console.log(games.filter(game => game.title.toLowerCase().includes("pokémon")).length))
