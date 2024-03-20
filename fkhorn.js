63c734df36bbf3b1f4f3a7328fd05e25f0f3138b
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-03-20 17:28:03 +0100

    test

--- fkhorn.js
+++ fkhorn.js
@@ -61,7 +61,7 @@ new cron.CronJob('00 * * * * *', () => {
     filtered = games.filter((game) => game.title.toLowerCase().includes("pokémon") || game.title.toLowerCase().includes("イナズマ"))
     filtered.length ?
       filtered.forEach(game => {
-        await res = `:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`
+        res = `:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`
         bot.channels.fetch("423919360902692866").then(chan => chan.send(res))
         //bot.channels.fetch("662216228340760596").then(chan => chan.send(`:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`))
         //webhook.send(`:flag_eu: \`${date_eu}\` **${game.title}** by __${game.developer}__ was found on the **European eShop**`)
