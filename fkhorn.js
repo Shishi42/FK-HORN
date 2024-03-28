3fa365c8d06e849b5f0df40f48a6eb2ae8dfbb62
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-03-28 15:43:54 +0100

    oups

--- fkhorn.js
+++ fkhorn.js
@@ -38,4 +38,18 @@ bot.on("interactionCreate", async (interaction) => {
   }
 })
 
+bot.player.events.on("playerStart", async (queue, track) => {
+  embed = new Discord.EmbedBuilder()
+    .setColor(bot.color)
+    .setDescription(`Starting playing : **${track.title}**.`)
+    .addFields({name: "Author", value: `${track.author}`})
+    .addFields({name: "Duration", value: `${track.duration}`})
+    .addFields({name: "Views", value: `${track.views}`})
+    .setThumbnail(track.thumbnail)
+    .setTimestamp()
+    .setFooter({text: `Song requested by ${track.requestedBy.username}`, iconURL: `${track.requestedBy.displayAvatarURL({dynamic: true})}`})
+  if(track.playlist) embed.addFields({name: "Playlist", value: `${track.playlist.title}`})
+  queue.metadata.channel.send({ embeds: [embed] })
+})
+
 bot.login(config.token)
